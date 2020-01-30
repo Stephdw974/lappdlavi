9221c71c7037ec6cc1b2ea6a93599ce1bc6db1a3
StephDw <StephDw@outlook.fr>
2020-01-30 12:41:44 +0100

    implement pincode auth

--- app/Http/Controllers/LappdlaviController.php
+++ app/Http/Controllers/LappdlaviController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class LappdlaviController extends Controller
 {
