1248a5abf9ca20954a16299d17f3df434be6fc3c
StephDw <StephDw@outlook.fr>
2020-01-30 18:52:23 +0100

    Fix background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
     <meta name="theme-color" content="#ffffff">
 </head>
 
-<body style="background: url('backgrounds/{{ Auth::user()->backgrounds->first()->image }}') no-repeat center fixed">
+<body @auth style="background: url('backgrounds/{{ Auth::user()->backgrounds->first()->image }}') no-repeat center fixed" @endauth>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-rb fixed-top">
             <div class="container">
